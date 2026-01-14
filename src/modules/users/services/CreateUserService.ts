803d591903b32a8018c6cb279406d62f4080bcca
Felipe <felipbarreto@icloud.com>
2026-01-13 23:34:53 -0300

    feat: create user service

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,32 @@
+import AppError from "@shared/errors/AppErrors";
+import { User } from "../database/entities/User";
+import { usersRepositories } from "../database/repositories/UsersRepositories";
+import { hash } from "bcrypt";
+
+interface ICreateUser{
+  name: string;
+  email: string;
+  password: string;
+}
+
+export default class CreateUserService {
+  async execute({name, email, password}: ICreateUser): Promise<User> {
+    const emailExist = await usersRepositories.findByEmail(email);
+
+    if(emailExist){
+      throw new AppError('Email adress already used.', 409);
+    }
+
+    const hashedPassword = await hash(password, 10)
+
+    const user = usersRepositories.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepositories.save(user);
+
+    return user;
+  }
+}
