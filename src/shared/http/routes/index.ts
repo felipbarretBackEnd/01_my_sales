a7e96a0c5ec443dffd7fbd5789b06c4741ea025c
Felipe <felipbarreto@icloud.com>
2026-01-18 22:16:01 -0300

    feat: users routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import productsRouter from '@modules/products/routes/ProductRoutes';
+import usersRouter from '@modules/users/routes/UserRoutes';
 import { Router } from 'express';
 
 import { request } from 'http';
@@ -9,5 +10,6 @@ routes.get('/health', (request, response) => {
   return response.json({ message: 'Hello Dev! I am Alive! '});
 });
 routes.use('/products', productsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
