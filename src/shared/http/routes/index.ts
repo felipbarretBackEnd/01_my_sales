62fef0850112e491e0e5a79c6cfec331ac33bfda
Felipe <felipbarreto@icloud.com>
2026-01-19 22:39:29 -0300

    fix: alter session prefix route

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -12,6 +12,6 @@ routes.get('/health', (request, response) => {
 });
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
-routes.use('sessions', sessionsRouter)
+routes.use('/sessions', sessionsRouter)
 
 export default routes;
